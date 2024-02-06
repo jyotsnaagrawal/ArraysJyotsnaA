696cbb076311b25e4b09dc7842efc24db9c3b9ba
Agrawal, Jyotsna <jyotsna.singhal432@gmail.com>
2024-02-06 10:02:44 -0600

    supplies for a simple hot dog stand

---
+++ src/main/kotlin/Main.kt
@@ -0,0 +1,9 @@
+fun main() {
+    // List of Variables
+    val supplies = arrayOf("Hot Dogs", "Buns", "Ketchup", "Mustard", "Onions")
+
+    // Use a for loop to print the contents of the array
+    for (supply in supplies) {
+        println(supply)
+    }
+}
